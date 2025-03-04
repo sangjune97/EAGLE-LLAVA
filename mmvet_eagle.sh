6e6936465c077bf65bf144d37b7378721aa684c6
sangjun <sangjune97@g.skku.edu>
2025-03-04 12:16:13 +0900

    first/last img token

--- mmvet_eagle.sh
+++ mmvet_eagle.sh
@@ -3,13 +3,13 @@ export CUDA_VISIBLE_DEVICES=0
 
 python -m model_vqa_eagle \
     --model-path llava-hf/llava-1.5-7b-hf \
-    --ea-model-path /home/sangjun/EAGLE-LLAVA/ckpt/finetune_w_img_5e-5/state_40 \
+    --ea-model-path /home/sangjun/EAGLE-LLAVA/ckpt/finetune_w_first_img_5e-5/state_40 \
     --question-file /home/sangjun/LLaVA/playground/data/eval/mm-vet/llava-mm-vet.jsonl \
     --image-folder /home/sangjun/LLaVA/playground/data/eval/mm-vet/images \
-    --answers-file /home/sangjun/EAGLE-LLAVA/ckpt/finetune_w_img_5e-5/state_40/test.jsonl \
+    --answers-file /home/sangjun/EAGLE-LLAVA/ckpt/finetune_w_first_img_5e-5/state_40/test.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1 \
-    --token-process 0 \
+    --token-process 3 \
 
 #0:nothing
 #1:remove
