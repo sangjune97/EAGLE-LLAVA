fca4df8c9c5da6e4d3b1aa1aa042d0a4a8dca34c
sangjun <sangjune97@g.skku.edu>
2025-03-23 22:40:38 +0900

    save

--- mmvet_eagle.sh
+++ mmvet_eagle.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1
 
 python -m model_vqa_eagle \
-    --model-path llava-hf/llava-1.5-7b-hf \
-    --ea-model-path /home/sangjun/EAGLE-LLAVA/ckpt/w_img_5e-5_fc2/state_32 \
+    --model-path llava-hf/llava-1.5-13b-hf \
+    --ea-model-path /home/sangjun/EAGLE-LLAVA/ckpt/not_finetune_w_img_1e-4_13b_only_hidden/state_20 \
     --question-file /home/sangjun/LLaVA/playground/data/eval/mm-vet/llava-mm-vet.jsonl \
     --image-folder /home/sangjun/LLaVA/playground/data/eval/mm-vet/images \
-    --answers-file /home/sangjun/EAGLE-LLAVA/ckpt/w_img_5e-5_fc2/state_32/mmvet.jsonl \
+    --answers-file /home/sangjun/EAGLE-LLAVA/ckpt/not_finetune_w_img_1e-4_13b_only_hidden/state_20.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1 \
-    --token-process 1 \
+    --token-process 0 \
 
 #0:nothing
 #1:remove
